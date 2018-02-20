0751d3a41d4f0c5dc55ae800b4dd3df5a20b5c13
Dan Phiffer <dan@phiffer.org>
2018-02-20 18:47:26 -0500

    :sparkles: basic files with some comments

---
+++ wp-content/themes/eyebeam2018/functions.php
@@ -0,0 +1,7 @@
+<?php
+/*
+
+Hello, this is the eyebeam2018 functions file.
+(20180220/dphiffer)
+
+*/
---
+++ wp-content/themes/eyebeam2018/index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+
+Oh hai, this is the eyebeam2018 theme. By default, this is the template that
+gets chosen. This is all written from scratch, with <3
+(20180220/dphiffer)
+
+*/
