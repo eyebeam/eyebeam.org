5b8f1457b540dbb0790a03f7e44f940c6b001379
Dan Phiffer <dan@phiffer.org>
2018-02-21 14:07:50 -0700

    :zap: align_items

--- wp-content/themes/eyebeam2018/js/eyebeam2018.js
+++ wp-content/themes/eyebeam2018/js/eyebeam2018.js
@@ -5,6 +5,7 @@ var eyebeam2018 = (function($) {
 		init: function() {
 			self.setup_nav();
 			self.setup_subscribe();
+			self.setup_items();
 		},
 
 		setup_nav: function() {
@@ -32,6 +33,41 @@ var eyebeam2018 = (function($) {
 						$form.addClass('error');
 					}
 				});
+			});
+		},
+
+		setup_items: function() {
+			self.align_items();
+			$(window).resize(self.align_items);
+		},
+
+		align_items: function() {
+			var span = 0;
+			var row = [];
+			$('.item-container .item').each(function(i, item) {
+				if ($(item).hasClass('one-third')) {
+					span += 1;
+				} else if ($(item).hasClass('two-thirds')) {
+					span += 2;
+				} else {
+					return;
+				}
+				$(item).css('height', 'auto');
+				if (span % 3 == 0) {
+					row.push(item);
+					var max_height = 0;
+					for (var i = 0; i < row.length; i++) {
+						if (row[i].offsetHeight > max_height) {
+							max_height = row[i].offsetHeight;
+						}
+					}
+					for (var i = 0; i < row.length; i++) {
+						$(row[i]).css('height', max_height + 'px');
+					}
+					row = [];
+				} else {
+					row.push(item);
+				}
 			});
 		}
 
