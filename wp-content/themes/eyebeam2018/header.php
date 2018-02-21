3d64ca2371a14f06db91ff9c2e805d0472b1557f
Dan Phiffer <dan@phiffer.org>
2018-02-20 20:28:40 -0500

    :tophat: header nav

--- wp-content/themes/eyebeam2018/header.php
+++ wp-content/themes/eyebeam2018/header.php
@@ -7,4 +7,13 @@
 	<body>
 		<nav>
 			<img src="<?php eyebeam2018_img_src('eyebeam.svg') ?>" alt="EYEBEAM" class="logo">
+			<ul class="menu">
+				<li>Apply</li>
+				<li>Learn</li>
+				<li>Visit</li>
+				<li>People</li>
+				<li>Ideas</li>
+				<li>About</li>
+			</ul>
+			<a href="#" class="support red">Support Eyebeam!</a>
 		</nav>
