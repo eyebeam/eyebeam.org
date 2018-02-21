9b61ecc879e81a2f2eaab29071208319af7a9784
Dan Phiffer <dan@phiffer.org>
2018-02-21 14:43:48 -0700

    :wrench: linebreaks

--- wp-content/themes/eyebeam2018/page.php
+++ wp-content/themes/eyebeam2018/page.php
@@ -11,11 +11,11 @@ while (have_posts()) {
 
 	the_post();
 
-	echo '<div class="item-container">';
+	echo "<div class=\"item-container\">\n";
 	while (have_rows('items')) {
 		get_template_part('templates/page-item');
 	}
-	echo '<br class="clear"></div>';
+	echo "<br class=\"clear\">\n</div>\n";
 }
 
 get_footer();
