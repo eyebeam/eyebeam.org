0efbbbf00dedc6a3efb9976fcfc675b73615bc1d
Dan Phiffer <dan@phiffer.org>
2018-02-21 14:46:33 -0700

    :information_desk_person: note about pages vs. posts

--- wp-content/themes/eyebeam2018/page.php
+++ wp-content/themes/eyebeam2018/page.php
@@ -1,7 +1,11 @@
 <?php
 /*
 
-General purpose page.php, which includes an ACF items repeater.
+Okay so if you are unfamiliar with WordPress, this is kind of ridiculous. In
+the dashboard you will notice there are both "posts" and "pages." This is the
+file that handles "pages." The main difference is that pages can be structured
+into a parent/child hierarchy, whereas posts are organized by when they were
+published (like a blog). (20180221/dphiffer)
 
 */
 
