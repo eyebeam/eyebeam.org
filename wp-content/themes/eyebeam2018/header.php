984fcfc6f0160c19c60de77809315f37d33697d9
Dan Phiffer <dan@phiffer.org>
2018-02-21 13:19:03 -0700

    :nail_care: wrap logo in H1

--- wp-content/themes/eyebeam2018/header.php
+++ wp-content/themes/eyebeam2018/header.php
@@ -7,7 +7,7 @@
 	<body>
 		<header>
 			<nav>
-				<img src="<?php eyebeam2018_img_src('img/eyebeam.svg'); ?>" alt="EYEBEAM" class="logo">
+				<h1><img src="<?php eyebeam2018_img_src('img/eyebeam.svg'); ?>" alt="EYEBEAM" class="logo"></h1>
 				<?php
 
 				// Edit nav items from the WordPress dashboard:
