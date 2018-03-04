15e633cb5dd00b91d6909cad0f96232e99f4f698
Dan Phiffer <dan@phiffer.org>
2018-03-04 12:04:49 -0500

    :bug: fix the wrong-template events bug

--- wp-content/themes/eyebeam2018/archive-event.php
+++ wp-content/themes/eyebeam2018/archive-event.php
@@ -2,11 +2,11 @@
 
 // We actually want the PAGE of this, not the ARCHIVE of this. I know, it is
 // super confusing, but the upshot is we don't need pagination, so we will go
-// with the page.php template. (20180303/dphiffer)
+// with the page-modular.php template. (20180303/dphiffer)
 
 query_posts(array(
 	'post_type' => 'page',
 	'name' => 'events'
 ));
 
-include_once('page.php');
+include_once('page-modular.php');
