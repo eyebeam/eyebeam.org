90ee20e4c80d8e2a4d2ffd7d6b3da42971a92cac
Dan Phiffer <dan@phiffer.org>
2018-03-11 09:28:48 -0400

    :wrench: dial down the ToC sizes

--- wp-content/themes/eyebeam2018/header.php
+++ wp-content/themes/eyebeam2018/header.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
 	</head>
-	<body>
+	<body <?php body_class(); ?>>
 		<header>
 			<nav>
 				<div class="menu-btn"></div>
