a67c8c25bcdf57031e1039b0b02d0622a2a91150
Dan Phiffer <dan@phiffer.org>
2018-03-13 11:38:13 -0400

    :link: link the resident name / photo
    re: #26

--- wp-content/themes/eyebeam2018/templates/collection-resident-item.php
+++ wp-content/themes/eyebeam2018/templates/collection-resident-item.php
@@ -7,6 +7,7 @@ $type = get_field('resident_type', $resident->ID);
 $start_year = get_field('start_year', $resident->ID);
 $end_year = get_field('end_year', $resident->ID);
 $image_id = get_field('image', $resident->ID);
+$links = get_field('links', $resident->ID);
 $collaboration_member = get_field('collaboration_member', $resident->ID);
 
 if ($collaboration_member == 'hide') {
@@ -19,13 +20,23 @@ if (! empty($image_id)) {
 	$image = eyebeam2018_get_image_html($image_id, $size, 'resident-image');
 }
 
-$bio_toggle = 'Bio';
+if (! empty($links)) {
+	$first_link = $links[0];
+	$url = $first_link['link_url'];
+	$name = "<a href=\"$url\">$name</a>";
+
+	if (! empty($image)) {
+		$image = "<a href=\"$url\">$image</a>";
+	}
+}
+
+/*$bio_toggle = 'Bio';
 $members = get_field('members', $resident->ID);
 if (! empty($members)) {
 	$bio_toggle = 'Bios';
-}
+}*/
 
-$bio = eyebeam2018_resident_bio($resident, $members);
+//$bio = eyebeam2018_resident_bio($resident, $members);
 
 $years = "$start_year&ndash;$end_year";
 
@@ -37,7 +48,7 @@ echo "<h4 class=\"resident-type person-title module-title\">$type</h4>\n";
 echo "<div class=\"resident-years\">$years</div>\n";
 //echo "<a href=\"#bio\" class=\"toggle-bio\">$bio_toggle</a>\n";
 //echo "<div class=\"bio\">$bio</div>\n";
-echo "$bio\n";
+//echo "$bio\n";
 echo "</div>\n";
 echo "</li>\n";
 
