95f2289b186daea363e64b492ee7e9abbec9add4
Emma Levesque <emma.levesque.schaefer@gmail.com>
2019-03-30 11:29:14 -0400

    adding new carousel and removing siema

--- wp-content/themes/eyebeam2019_alt/templates/item-carousel.php
+++ wp-content/themes/eyebeam2019_alt/templates/item-carousel.php
@@ -1,20 +1,24 @@
 <?php
 
 $carousel = get_sub_field('carousel');
-
-echo "<div class=\"carousel\">\n";
+echo "<div class=\"carousel-container\">\n";
+echo "<div class=\"swiper-wrapper\">\n";
 // show and float all media
 if ($carousel){
 
 	foreach($carousel as $row){
 			$target = get_permalink($row['slide_target'][0]);
 			$image = eyebeam2018_get_image_html($row['image'], 'large', 'post-image');
-			echo "<figure class=\"slide\">\n";
+			echo "<div class=\"swiper-slide\">\n";
 			echo "<a href=\"$target\">\n";
 			echo "$image\n";
 			echo "</a>\n";
-			echo "</figure>\n";
+			echo "</div>\n";
 
 	}
 }
+echo "</div>";
+echo "<div class=\"swiper-pagination\"></div>\n";
+echo "<div class=\"swiper-button-prev swiper-button-right\"></div>\n";
+echo "<div class=\"swiper-button-next swiper-button-right\"></div>\n";
 echo "</div>";
