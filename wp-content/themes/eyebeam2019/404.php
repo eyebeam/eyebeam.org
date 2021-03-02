1900f61eabd47643f3ec3ac178292d4e2b88976c
Emma Levesque <emma.levesque.schaefer@gmail.com>
2021-03-01 19:15:19 -0500

    adding 404 page

---
+++ wp-content/themes/eyebeam2019/404.php
@@ -0,0 +1,51 @@
+<?php
+/*
+
+Oh hai, this is the eyebeam2018 theme. By default, this is the template that
+gets chosen. This is all written from scratch, with <3
+(20180220/dphiffer)
+
+Some basic ground rules:
+- I don't conform to WordPress style guide, because it looks gross to my eyes.
+- I use tabs for indenting, and spaces to align things. It's fussy, I know.
+- Underscores for naming_things instead of camelCase.
+- Try to keep everything self-contained within the theme code base as much as
+  possible.
+- We put things in $GLOBALS['eyebeam2018'] to make things easier.
+- Develop with WP_DEBUG enabled in wp-config.php, otherwise turn it off.
+- Default to the simplest dumbest thing.
+(20180303/dphiffer)
+
+Some places you might want to look next:
+* templates/post.php
+* header.php and footer.php
+* functions.php
+* style.css (no preprocessing magic, just straight up CSS)
+* js/eyebeam2018.js
+* lib/*
+
+For a general discussion on how WordPress themes work, you might want to check
+out these articles:
+https://codex.wordpress.org/Theme_Development
+https://developer.wordpress.org/themes/getting-started/
+
+(20180307/dphiffer)
+
+*/
+
+get_header();
+the_post();
+
+?>
+<div class="post">
+    <div class="post-main">
+        <h2 class="post-title eyebeam-sans">Page not found</h2>
+        <div class="post-content">
+            <p>It looks like the URL you entered isn't valid. Try searching for what you want or navigate to our <a href="https://eyebeam.org">home page</a>
+        </div>
+    </div>
+</div>
+
+<?php 
+
+get_footer();
