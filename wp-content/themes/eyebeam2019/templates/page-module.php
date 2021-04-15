ceedc488a26c3667144791301f360a698b583ba7
Emma Levesque <emma.levesque.schaefer@gmail.com>
2021-04-15 16:41:52 -0400

    fixing issue with module-hash

--- wp-content/themes/eyebeam2019/templates/page-module.php
+++ wp-content/themes/eyebeam2019/templates/page-module.php
@@ -19,6 +19,10 @@ if (! empty($toc_title)) {
 	$hash = sanitize_title($toc_title);
 }
 
+if (! empty($section_header)) {
+	$hash = sanitize_title($section_header);
+}
+
 // if it's a separator display it
 if ($type == 'separator'){
 	echo "<hr />";
@@ -45,7 +49,7 @@ else {
 	if (! empty($image_id)) {
 
 		// set the image size
-		$size = 'large';
+		$size = 'medium';
 		// get the image using our helper function
 		$image = eyebeam2018_get_image_html($image_id, $size, true, $url);
 
@@ -55,14 +59,24 @@ else {
 
 	}
 
-
+	// if section header exists, overwrite the title
+	// @todo change this
+	// print section header module
 	if ($section_header != ""){
+
 		$title = $section_header;
+
 		if (!empty($section_header_link)){
 			$link = $section_header_link;
 		}
-		echo (!empty($link)) ? "<h2 class=\"module-title\"><a href=\"$link\">$title</a></h2>" : "<h2 class=\"module-title\">$title</h2>";
-		echo (!empty($link)) ?  "<a class=\"btn\" href=\"$link\">Read More</a>" : "";
+
+		echo (!empty($link)) 
+			? "<h2 class=\"module-title section-header\"><a href=\"$link\">$title</a></h2>" 
+			: "<h2 class=\"module-title section-header\">$title</h2>";
+
+		echo (!empty($link)) 
+			?  "<a class=\"btn\" href=\"$link\">Read More</a>" 
+			: "";
 	}
 
 	// if there's a title
@@ -74,22 +88,29 @@ else {
 
 		// if there's a link set
 		if ( !empty($url) ){
-			$title_text = ($show_button == 'show') ? $title : "<a aria-label=\"Read More about $title\" title=\"$title\" href=\"$url\">$title</a>";
+			$title_text = ($show_button == 'show') 
+				? $title 
+				: "<a aria-label=\"Read More about $title\" title=\"$title\" href=\"$url\">$title</a>";
 		}
 		else {
 			$title_text = $title;
 		}
 
 		// set the description
-		$text_description .= (empty($section_header)) ? "<h2 class=\"module-title eyebeam-sans\" alt=\"$title\" title=\"$title\">$title_text</h2>\n" : "";
+		$text_description .= (empty($section_header)) 
+			? "<h2 class=\"module-title eyebeam-sans\" alt=\"$title\" title=\"$title\">$title_text</h2>\n" 
+			: "";
 	}
 
 
 	// if there's a subtitle
 	if (! empty($subtitle)){
-		$text_description .= "<h3 class=\"module-subtitle eyebeam-sans\" alt=\"$subtitle\" title=\"$subtitle\">" . $subtitle . "</h3>";
+		$text_description .= "<h3 class=\"module-subtitle eyebeam-sans\" alt=\"$subtitle\" title=\"$subtitle\">" 
+							. $subtitle 
+							. "</h3>";
 	}
 
+	// if there's a description
 	if (! empty($description)) {
 		$text_description .= "<div class=\"module-description\">$description\n";
 	}
