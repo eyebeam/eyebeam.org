0c9f2e649534957a1651aafb60b4230557d1dcd2
Emma Levesque <emma.levesque.schaefer@gmail.com>
2021-12-16 15:51:05 -0500

    updating donate module

--- wp-content/themes/eyebeam2019/templates/page-module-donate.php
+++ wp-content/themes/eyebeam2019/templates/page-module-donate.php
@@ -1,16 +1,44 @@
-<li id="module-donate" class="module module-donate module-one_third">
+<li id="module-donate" class="module module-donate module-full_width">
 
-	<h2>
-		Make a Cash Donation
-	</h2>
-	<!-- Begin Give Lively Fundraising Widget -->
-		<script>gl=document.createElement('script');gl.src='https://secure.givelively.org/widgets/simple_donation/the-eyebeam-atelier-inc/donate-to-eyebeam.js?show_suggested_amount_buttons=false&show_in_honor_of=false&address_required=false&has_required_custom_question=false';document.getElementsByTagName('head')[0].appendChild(gl);</script><div id="give-lively-widget" class="gl-simple-donation-widget"></div>
-	<!-- End Give Lively Fundraising Widget -->
+	<div>
+		<label for="cash-donation">
+			<h2>
+				Make a Cash Donation
+			</h2>
+		</label>
+		<input type="checkbox" id="cash-donation" name="cash-donation" />
+		<div>
+			<!-- Begin Give Lively Fundraising Widget -->
+				<script>gl=document.createElement('script');gl.src='https://secure.givelively.org/widgets/simple_donation/the-eyebeam-atelier-inc/donate-to-eyebeam.js?show_suggested_amount_buttons=false&show_in_honor_of=false&address_required=false&has_required_custom_question=false';document.getElementsByTagName('head')[0].appendChild(gl);</script><div id="give-lively-widget" class="gl-simple-donation-widget"></div>
+			<!-- End Give Lively Fundraising Widget -->
+		</div>
+	</div>
 
-	<h2>
-		Make a Crypto Donation
-	</h2>
-	<!-- Begin Gemini Fundraising widget -->
-	<script id="tgb-widget-script"> !function(t,e,i,n,o,c,d,s){t.tgbWidgetOptions={id:o,domain:n},(d=e.createElement(i)).src=[n,"widget/script.js"].join(""),d.async=1,(s=e.getElementById(c)).parentNode.insertBefore(d,s)}(window,document,"script","https://tgbwidget.com/","133952075","tgb-widget-script"); </script>
-	<!-- End Gemini Fundraising widget -->
+	<div>
+		<label for="crypto-donation">
+			<h2>
+				Make a Crypto Donation
+			</h2>
+		</label>
+		<input type="checkbox" id="crypto-donation" name="crypto-donation" />
+		<div>
+			<!-- Begin Gemini Fundraising widget -->
+			<script id="tgb-widget-script"> !function(t,e,i,n,o,c,d,s){t.tgbWidgetOptions={id:o,domain:n},(d=e.createElement(i)).src=[n,"widget/script.js"].join(""),d.async=1,(s=e.getElementById(c)).parentNode.insertBefore(d,s)}(window,document,"script","https://tgbwidget.com/","133952075","tgb-widget-script"); </script>
+			<!-- End Gemini Fundraising widget -->
+		</div>
+	</div>
+
+	<div>
+
+		<label for="stock-donation">
+			<h2>
+				Make a Stock Donation
+			</h2>
+		</label>
+		<input type="checkbox" id="stock-donation" name="stock-donation" />
+		<div>
+			<link rel="stylesheet" type="text/css" media="all" href="https://stockdonator.com/widget.css" /><a href="https://stockdonator.com/stock-information/?oid=cfff70ff" target="_blank" class="widget-btn btn-orange" >Donate Stock To Eyebeam Atelier, Inc</a>
+		</div>
+	</div>
+
 </li>
